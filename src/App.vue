96df8e61111e06daa6aa9a6b6920704c270d6f77
szc0925 <1029243083@qq.com>
2022-08-27 22:26:41 +0800

    feat(初始化): 修改了eslint配置 + 引入iconfont字体图标

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-const a = '34';
-const b = 123;
-console.log(a, b);
+	const a = '34';
+	const b = 123;
+	console.log(a, b);
 </script>
-
 <template>
-    <div>Hello</div>
+	<div>Hello</div>
+	<i class="iconfont">&#xe7eb;</i>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './reset.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import './reset.css';
+import App from './App.vue';
+import './assets/font/iconfont.css';
+import './assets/font/iconfont.js';
+createApp(App).mount('#app');
